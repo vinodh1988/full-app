4253ca1f5c592679911ee9921d81ba699ec9e601
vinodh1988 <k.c.vinodh@hotmail.com>
2023-02-24 11:56:09 +0530

    sercommit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HomeService } from 'src/app/services/home.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  services:any;  
+  constructor(public hs:HomeService){
+        hs.getServices().subscribe(
+              {
+                next: (data:any)=>this.services=data,
+                error: ()=>this.services=[]
+              }
+        )
+    }
 }
--- src/app/pages/home/services/services.component.ts
+++ src/app/pages/home/services/services.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component , Input} from '@angular/core';
 
 @Component({
   selector: 'app-services',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./services.component.css']
 })
 export class ServicesComponent {
-
+ @Input('service') current:any;
 }
